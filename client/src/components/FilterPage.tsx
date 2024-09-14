9bfe62693a47ae0cc85527a7a4f5dbcbe7bdd8da
simallworld <shivamswaroop89@gmail.com>
2024-09-15 04:57:09 +0530

    Whole Search page done with filter functionality, just starting Reastaurant detail page

--- client/src/components/FilterPage.tsx
+++ client/src/components/FilterPage.tsx
@@ -16,9 +16,7 @@ const filterOptions: FilterOptionsState = [
 
 const FilterPage = () => {
 
- const appliedFilterHandler = (value:string) => {
-      alert(value);
- }  
+ const appliedFilterHandler = (value:string) => {}  
 
   return (
     <div className="md:w-72">
